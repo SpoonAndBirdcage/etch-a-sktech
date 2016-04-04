69c9977c8b084d51a80b10891b74676ca4d2ef99
SpoonAndBirdcage <spoonandbirdcage@gmail.com>
2016-04-04 14:52:01 -0600

    Set Grid is working so is reset and all 4 colors. Need to figure out trail, darken and random colors

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-  var box_count = 64;
-  for (var i = 0; i < box_count; i++) {
-    for (var j = 0; j < box_count; j++) {
-      $('.wrapper').append('<div class="square"></div>');
+  /* Used To Set Up Grid */
+  function gridSetup() {
+    var box_count = $('.grid-num').val();
+    var box_face = ((700 / box_count) - 1) + 'px';
+    for (var i = 0; i < box_count; i++) {
+      for (var j = 0; j < box_count; j++) {
+        $('.wrapper').append('<div class="square" style="width:' + box_face + '; height: ' + box_face + ';"></div>');
+      }
     }
+    $('div').filter('.selected').click();
   }
-
   /* Used To Reset All Color Buttons */
   function btnReset() {
-    return $('.control').children().removeClass('yellow blue purple green');
+    $('.control').children().removeClass('yellow blue purple green');
+  }
+
+  function selectedBtn(btnClass) {
+    $('.selected').removeClass('selected');
+    $(btnClass).addClass('selected');
   }
 
   /* Used Switch Between colors with buttons */
@@ -23,18 +32,22 @@ $(document).ready(function() {
 
   /* The Yellow-Toggle Button */
   $('.tog-1').on('click', function() {
+    selectedBtn('.tog-1');
     colorSwitch('.tog-1', 'yellow', 'blue purple green');
   });
   /* The Blue-Toggle Button */
   $('.tog-2').on('click', function() {
+    selectedBtn('.tog-2');
     colorSwitch('.tog-2', 'blue', 'yellow purple green');
   });
   /* The Purple-Toggle Button */
   $('.tog-3').on('click', function() {
+    selectedBtn('.tog-3');
     colorSwitch('.tog-3', 'purple', 'yellow blue green');
   });
   /* The Purple-Toggle Button */
   $('.tog-4').on('click', function() {
+    selectedBtn('.tog-4');
     colorSwitch('.tog-4', 'green', 'yellow blue purple');
   });
   /* The Reset Button */
@@ -44,9 +57,22 @@ $(document).ready(function() {
     $('.tog-1').click();
   });
 
+  $('.set-grid').on('click', function(e) {
+    e.preventDefault();
+    $('div.square').remove();
+    if ($('.grid-num').val() > 80) {
+      $('.grid-num').val(80);
+      gridSetup();
+    } else if ($('.grid-num').val() < 5) {
+      $('.grid-num').val(5);
+      gridSetup();
+    } else {
+      gridSetup();
+    }
+  });
 
   /*
-    Default starting position set on load
+    Setting Default Start on Load
   */
-  $('.tog-1').click();
+  gridSetup();
 });
